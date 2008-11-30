d229ddcb3c442832bb32bba0611fa4fa9544f64d
Michael Wilber <hotdog003@gmail.com>
2008-11-29 20:52:31 -0700

    Transferred all of my notes to Tomboy

--- jquery.texteditor.js
+++ jquery.texteditor.js
@@ -18,15 +18,37 @@ jQuery.fn.document = function(server, documentnumber, nick) {
     function enableDesignMode(iframe) {
         // This function makes a single iframe editable.
         iframe.contentWindow.document.open();
-        iframe.contentWindow.document.write("<html><body><br /></body></html>");
+        iframe.contentWindow.document.write("<html><head>" + 
+        "<link href='design.css' rel='stylesheet' type='text/css'>" +
+        "</head><body></body></html>");
         iframe.contentWindow.document.close();
         
-        iframe.contentWindow.document.designMode = "on";
+        //iframe.contentWindow.document.designMode = "on";
+        // Seems like a better way- set contentEditable to true. Note: This works
+        // perfectly on IE.
+        $("body", iframe.contentWindow.document).attr("contentEditable", true);
+        $("body", iframe.contentWindow.document).text("Editable Text ");
+        $("body", iframe.contentWindow.document).append("<span class='notype'>Non-editable text</span>");
+        $("body", iframe.contentWindow.document).append(" Editable Text");
         
-        // Now that we have a nice editable iframe, let's add some handlers!
-        $(iframe.contentWindow).keypress(function(event) {
-            //alert(event.keyCode);
+        $(".notype", iframe.contentWindow.document).attr("contentEditable", false);
+        /* This was a way to keep the user from changing other people's words. However,
+        it seems that it was a bit too... well... hackish. Oh well. Perhaps it
+        was the best solution after all. 
+        $(iframe.contentWindow.document).keypress(function(event) {
+            // Prevent typing in that space
+            // Compatability: Fx2: TODO; Fx3: Perfect; Ie6: TODO; Ie7: 
+            
+            // TODO: Fix this working with nested elements
+            // BUG: This function fails if you right-click and choose "paste"
+            
+            if ($(iframe.contentWindow.getSelection().anchorNode).parent().is(".notype") && event.charCode != 0) {
+                // TODO: Instead of preventing tying, just split the chunks up
+                event.preventDefault();
+                    
+            }
         });
+        */
     };
 
 }
